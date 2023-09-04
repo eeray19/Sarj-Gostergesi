3d5183db48c8fe9a8e78ab8be1219f24c2a5c686
eeray19 <56322566+eeray19@users.noreply.github.com>
2023-09-04 15:26:30 +0300

    update battery info without refreshing the page

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-//using the BatteryManager object
-
-initBattery();
-
 function initBattery() {
-
-    //selecting HTML elements
     const renk = document.querySelector(".renk");
     const sarjDurumu = document.querySelector(".sarjDurum");
     const yuzde = document.querySelector(".yuzde");
 
+    function updateBattery(batt) {
+        let level = Math.floor(batt.level * 100);
+        yuzde.innerHTML = "%" + level;
 
-    navigator.getBattery().then(
-        (batt) => { //arrow function 
-        updateBattery = () => { //arrow function 
-            let level = Math.floor(batt.level * 100); 
-            yuzde.innerHTML = "%" + level;
+        renk.style.height = `${parseInt(batt.level * 100)}%`;
 
-            renk.style.height = `${parseInt(batt.level * 100)}%`; 
-
-            if (level == 100) { 
-                sarjDurumu.innerHTML = `Tam Dolu <i class="ri-battery-2-fill green-color"></i>`;
-                renk.style.height = "103%";
-            } else if (level <= 20 & !batt.charging) {
-                sarjDurumu.innerHTML = `Düşük Şarj <i class="ri-plug-line animated-red animated-red"></i>`;
-            } else if (batt.charging) {
-                sarjDurumu.innerHTML = `Şarj Ediliyor ... <i class="ri-flashlight-line animated-green"></i>`;
-            } else {
-                sarjDurumu.innerHTML = "";
-            }
-
-            if (level <= 20) { 
-                renk.classList.add("kirmizi");
-                renk.classList.remove("yesil", "turuncu", "sari");
-            } else if (level <= 48) {
-                renk.classList.add("turuncu");
-                renk.classList.remove("yesil", "kirmizi", "sari");
-            } else if (level <= 80) {
-                renk.classList.add("sari");
-                renk.classList.remove("yesil", "turuncu", "kirmizi");
-            } else {
-                renk.classList.add("yesil");
-                renk.classList.remove("kirmizi", "turuncu", "sari");
-            }
+        if (level == 100) {
+            sarjDurumu.innerHTML = `Tam Dolu <i class="ri-battery-2-fill green-color"></i>`;
+            renk.style.height = "103%";
+        } else if (level <= 20 && !batt.charging) {
+            sarjDurumu.innerHTML = `Düşük Şarj <i class="ri-plug-line animated-red animated-red"></i>`;
+        } else if (batt.charging) {
+            sarjDurumu.innerHTML = `Şarj Ediliyor ... <i class="ri-flashlight-line animated-green"></i>`;
+        } else {
+            sarjDurumu.innerHTML = "";
         }
-        updateBattery();
-        batt.addEventListener("chargingchange", () => { updateBattery() });
-        batt.addEventListener("levelchange", () => { updateBattery });
+
+        if (level <= 20) {
+            renk.classList.add("kirmizi");
+            renk.classList.remove("yesil", "turuncu", "sari");
+        } else if (level <= 48) {
+            renk.classList.add("turuncu");
+            renk.classList.remove("yesil", "kirmizi", "sari");
+        } else if (level <= 80) {
+            renk.classList.add("sari");
+            renk.classList.remove("yesil", "turuncu", "kirmizi");
+        } else {
+            renk.classList.add("yesil");
+            renk.classList.remove("kirmizi", "turuncu", "sari");
+        }
     }
 
-    )
+    navigator.getBattery().then((batt) => {
+        // initial update
+        updateBattery(batt);
+
+        // update battery without refreshing the page
+        batt.addEventListener("chargingchange", () => {
+            updateBattery(batt);
+        });
+        batt.addEventListener("levelchange", () => {
+            updateBattery(batt);
+        });
+    });
 }
+
+initBattery();
